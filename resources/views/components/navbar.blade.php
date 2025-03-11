f6cebbb0d9c5ec5144b9a174955cb4d5d26b6714
Rustam650 <spaces553@mail.ru>
2025-03-11 17:34:50 +0300

    Исправление стилей: модальное окно, бургер-меню, адаптивность и логотип

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<!-- Навигационная панель -->
+<nav class="navbar">
+    <div class="navbar-container">
+        <a href="/" class="navbar-logo">
+            <img src="{{ asset('images/logo.svg') }}" alt="Stone Hill" class="logo-color">
+            <img src="{{ asset('images/logow.svg') }}" alt="Stone Hill" class="logo-white">
+        </a>
+        
+        // ... existing code ...
+    </div>
+</nav> 
