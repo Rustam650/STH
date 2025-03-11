0be00240a59146aed79b4db9455552ec1c3e0860
Rustam650 <spaces553@mail.ru>
2025-03-11 16:27:28 +0300

    Принудительное использование HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,10 @@ class Kernel extends HttpKernel
      * @var array<int, string>
      */
     protected $middleware = [
+        \App\Http\Middleware\ForceHttps::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
